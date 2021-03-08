a4ea5bfa5da0becf3aea7a2d21183a8b66349380
Lana Karazoun <lkarazoun@usf.edu>
2021-03-08 15:33:43 -0500

    Add project files.

---
+++ Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using System.IO;
+
+namespace Deliverable7
+{
+    [ComVisible(true)]
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.Load += new EventHandler(Form1_Load);
+            this.webBrowser1.DocumentCompleted += new WebBrowserDocumentCompletedEventHandler(webBrowser1_DocumentCompleted);
+
+            //call the c# code in java script function
+            webBrowser1.ObjectForScripting = this;
+            webBrowser1.ScriptErrorsSuppressed = false;
+
+            //disable right click on web browser control 
+            webBrowser1.IsWebBrowserContextMenuEnabled = false;
+            webBrowser1.AllowWebBrowserDrop = false;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //Below you have to get the current directory of this application
+            string CurrentDirectory = Directory.GetCurrentDirectory();
+            webBrowser1.Navigate(Path.Combine(CurrentDirectory, "HTMLPageForJavaScript.html"));
+        }
+        private void Report()
+        {
+            //Get HTML page div from id of Div.
+            HtmlElement div = webBrowser1.Document.GetElementById("reportContent");
+
+            //create a simple html content
+            StringBuilder sb = new StringBuilder();
+            sb.Append("<table>");
+            sb.Append("<tr><td><B> Hi this is my report demo</B></td></tr>");
+            sb.Append("</table>");
+
+            //assign content to teh HTMl page div
+            div.InnerHtml = sb.ToString();
+        }
+
+        private void webBrowser1_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+            //when form is load cursor focus on the web browser control
+            webBrowser1.Focus();
+
+            //call report method which contains the report content
+            Report();
+        }
+        public void PrintReport()
+        {
+            //show print dialog and call print method of webrowser control
+            DialogResult dr = printDialog1.ShowDialog();
+
+            if (dr.ToString() == "OK")
+            {
+                webBrowser1.Print();
+            }
+            else
+            {
+                return;
+            }
+        }
+    }
+}
